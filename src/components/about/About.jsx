088f82afd0b86fef3509cde3e747da8de8d51a44
br1lisboa <brlisbo@gmail.com>
2022-06-16 12:25:30 -0300

    finish about section

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,5 +1,15 @@
 import React from 'react'
 import About1 from '../../img/img-about1.jpg'
+import logoJS from '../../img/logoJS.png'
+import logoREACT from '../../img/logoREACT.png'
+import logoBTS from '../../img/logoBTS.png'
+import logoCSS from '../../img/logoCSS.png'
+import logoGIT from '../../img/logoGIT.png'
+import logoGITHUB from '../../img/logoGITHUB.png'
+import logoGITKRAKEN from '../../img/logoGITKRAKEN.png'
+import logoHTML from '../../img/logoHTML.png'
+import logoTAILWIND from '../../img/logoTAILWIND.png'
+import logoSASS from '../../img/logoSASS.png'
 import './about.css'
 
 function About() {
@@ -13,17 +23,45 @@ function About() {
             </div>
         </div>
         <div className="a-right">
-            <h1 className="i-title">About Me</h1>
-            <p className="a-sub">
-                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Repellat mollitia inventore ipsa ab, nobis, cupiditate praesentium nesciunt doloremque amet in at assumenda! Voluptatum reiciendis itaque, doloremque sed ducimus ex enim.
-            </p>
-            <p className="a-des">
-                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Magnam quod quaerat temporibus nulla excepturi fugiat animi! Sapiente, similique consequatur quisquam sed provident minus illo, illum beatae sunt impedit fugit quod. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quisquam facere dolorum, eveniet ullam minima sit, quidem optio impedit, aperiam mollitia perferendis id. Perspiciatis animi esse eum molestiae est minus suscipit!
-            </p>
-        </div>
-        <div className="a-award">
-            <img src="" alt="" className="a-award-img" />
-        </div>
+            <div className="a-right-top">
+                <h1 className="i-title">Skills</h1>
+                <p className="a-sub">
+                    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Repellat mollitia inventore ipsa ab, nobis, cupiditate praesentium nesciunt doloremque amet in at assumenda! Voluptatum reiciendis itaque, doloremque sed ducimus ex enim.
+                </p>
+            </div>
+            <div className="a-right-bot">
+                <div className="a-award">
+                    <img src={logoJS} alt="" className="a-award-img" />
+                </div>
+                <div className="a-award">
+                    <img src={logoREACT} alt="" className="a-award-img" />
+                </div>
+                <div className="a-award">
+                    <img src={logoHTML} alt="" className="a-award-img" />
+                </div>
+                <div className="a-award">
+                    <img src={logoCSS} alt="" className="a-award-img" />
+                </div>
+                <div className="a-award">
+                    <img src={logoSASS} alt="" className="a-award-img" />
+                </div>
+                <div className="a-award">
+                    <img src={logoBTS} alt="" className="a-award-img" />
+                </div>
+                <div className="a-award">
+                    <img src={logoTAILWIND} alt="" className="a-award-img" />
+                </div>
+                <div className="a-award">
+                    <img src={logoGIT} alt="" className="a-award-img" />
+                </div>
+                <div className="a-award">
+                    <img src={logoGITKRAKEN} alt="" className="a-award-img" />
+                </div>
+                <div className="a-award">
+                    <img src={logoGITHUB} alt="" className="a-award-img" />
+                </div>
+            </div>
+        </div>   
     </div>
   )
 }
